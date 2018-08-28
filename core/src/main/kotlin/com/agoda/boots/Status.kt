9f58d576839a9b66826ad99a1e2fdfcc7851e16c
Ilya Lim <ilya.lim@agoda.com>
2018-08-28 12:38:21 +0700

    KDocs for Status

--- core/src/main/kotlin/com/agoda/boots/Status.kt
+++ core/src/main/kotlin/com/agoda/boots/Status.kt
@@ -1,24 +1,67 @@
 package com.agoda.boots
 
+/**
+ * Represents the status of a given [Bootable] within the boot system.
+ * Status is returned as a property of [Report] data class returned by the
+ * [Boots.report].
+ */
 sealed class Status {
 
+    /**
+     * Idle status means that [Bootable] hasn't been touched yet by the boot system.
+     * All freshly added [Bootable]s through [Boots.add] get the idle status automatically.
+     */
     class Idle : Status()
+
+    /**
+     * Booting status means that [Bootable] has been added to the [Executor]'s queue and will
+     * start executing/executing right now.
+     */
     class Booting : Status()
+
+    /**
+     * Booted status means that [Bootable] has finished it's [Bootable.boot] invocation
+     * without throwing any exception, thus meaning that it has booted successfully.
+     */
     class Booted : Status()
+
+    /**
+     * Failed status means that [Bootable] threw an exception during [Bootable.boot]
+     * invocation, thus meaning that it hasn't booted successfully.
+     *
+     * @param reason exception that the [Bootable.boot] function has thrown.
+     */
     class Failed(val reason: Throwable) : Status()
 
     companion object {
+        /**
+         * Creates an instance of [Idle] status
+         * @return instance of [Idle]
+         */
         @JvmStatic
         fun idle(): Status = Status.Idle()
 
+        /**
+         * Creates an instance of [Booting] status
+         * @return instance of [Booting]
+         */
         @JvmStatic
         fun booting(): Status = Status.Booting()
 
+        /**
+         * Creates an instance of [Booted] status
+         * @return instance of [Booted]
+         */
         @JvmStatic
         fun booted(): Status = Status.Booted()
 
+        /**
+         * Creates an instance of [Failed] status
+         * @param throwable reason of failure
+         * @return instance of [Failed]
+         */
         @JvmStatic
-        fun failed(throwable: Throwable): Status = Status.Failed(throwable)
+        fun failed(reason: Throwable): Status = Status.Failed(reason)
     }
 
 }
