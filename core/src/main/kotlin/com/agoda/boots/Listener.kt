4c08cb2c38104013fd40c8417b652cfece4e40ff
Ilya Lim <ilya.lim@agoda.com>
2018-08-28 14:19:09 +0700

    KDocs for Listener

--- core/src/main/kotlin/com/agoda/boots/Listener.kt
+++ core/src/main/kotlin/com/agoda/boots/Listener.kt
@@ -1,11 +1,32 @@
 package com.agoda.boots
 
+/**
+ * Callback class to listen results of a boot process.
+ */
 class Listener {
+    /**
+     * This function is invoked when boot process has finished successfully.
+     */
     var onBoot: (Report) -> Unit = {}
+
+    /**
+     * This function is invoked when boot process hasn't finished successfully.
+     */
     var onFailure: (Report) -> Unit = {}
 
+    /**
+     * Interface providing Java style means to easily instantiate [Listener] via
+     * anonymous inner classes.
+     */
     interface Builder {
+        /**
+         * This function is invoked when boot process has finished successfully.
+         */
         fun onBoot(report: Report)
+
+        /**
+         * This function is invoked when boot process hasn't finished successfully.
+         */
         fun onFailure(report: Report)
     }
 
