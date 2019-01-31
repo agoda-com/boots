d425698ec352aea1ecf2d20cd66d6752095c05a6
a.fialko <a.fialko@corp.mail.ru>
2019-01-31 13:40:43 +0300

    use default executor

--- executor/executor-pure-android/src/main/kotlin/com/agoda/NativeAndroidExecutor.kt
+++ executor/executor-pure-android/src/main/kotlin/com/agoda/NativeAndroidExecutor.kt
@@ -4,7 +4,7 @@ import android.os.Handler
 import android.os.Looper
 import com.agoda.boots.Executor
 import com.agoda.boots.Executor.Companion.DEFAULT_CAPACITY
-import java.util.concurrent.ExecutorService
+import com.agoda.boots.impl.DefaultExecutor
 
 /**
  * Native implementation of [Executor] with Android support.
@@ -12,17 +12,16 @@ import java.util.concurrent.ExecutorService
  * Can switch to Android's main thread context to invoke listeners
  * and [non-concurrent][com.agoda.boots.Bootable.isConcurrent] bootables.
  */
-class NativeAndroidExecutor @JvmOverloads constructor(
-    private val executor: ExecutorService,
+class AndroidExecutor @JvmOverloads constructor(
     override val capacity: Int = DEFAULT_CAPACITY
-) : Executor {
+) : DefaultExecutor() {
 
     private val handler = Handler(Looper.getMainLooper())
     override val isMainThreadSupported = true
 
     override fun execute(isConcurrent: Boolean, executable: () -> Unit) {
         if (isConcurrent) {
-            executor.submit(executable)
+            pool.submit(executable)
         } else {
             handler.post(executable)
         }
